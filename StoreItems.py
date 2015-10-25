6805f25813a9b3caac5b70b82b842097d0ff5590
B Baker <bakerbran89@gmail.com>
2015-10-25 13:28:37 -0700

    Store items from UpslideDown drift team
    Used Flask, JQuery, Bootstrap.  DB is next - not sure how to go about
    this.

---
+++ StoreItems.py
@@ -0,0 +1,19 @@
+﻿"""uses inheritance based on item type."""
+class StoreItems(object):
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+
+class Apparel(StoreItems):
+    def __init__(self, name, price, description, sizes, colors):
+        self.sizes = sizes
+        self.colors = colors
+        StoreItems.__init__(self, name, price, description)
+
+class Part(StoreItems):
+    def __init__(self, name, price, description, dimensions, make, model):
+        self.dimensions = dimensions
+        self.make = make
+        self.model = model
+        StoreItems.__init__(self, name, price, description)
---
+++ views.py
@@ -0,0 +1,54 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from UpslideDownGarage import app
+#from UpslideDownGarage.StoreItems import StoreItems, Apparel, Part
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template('HomePage.html',
+        title='Home',
+        year=datetime.now().year,
+        message='Message!.')
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template('contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.')
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template('about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.')
+
+@app.route('/shop')
+def shop():
+    return render_template('Shop.html')
+
+@app.route('/parts')
+def parts():
+    return render_template('Parts.html',
+        year=datetime.now().year)
+
+@app.route('/apparel')
+def apparel():
+        return render_template('Apparel.html',       
+        year=datetime.now().year
+        )
+
+@app.route('/accessories')
+def accessories():
+        return render_template('Accessories.html',       
+        year=datetime.now().year,
+        )
